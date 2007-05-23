ac1a45af08d65f5545554a9a392323e1171bfa83
jana <jana>
2007-05-23 13:24:32 +0000

    JO-2007-05-23-0: RFE:         Arco should support webconsole 3.0.x                  Bugster:     6481737                  Changed:     arcorun, inst_reporting, build.xml, build.properties,                               util, src                  Review:      AP

--- source/reporting/src/com/sun/grid/arco/web/arcomodule/ArcoServlet.java
+++ source/reporting/src/com/sun/grid/arco/web/arcomodule/ArcoServlet.java
@@ -57,28 +57,12 @@ public class ArcoServlet extends ArcoServletBase {
      * this simple example. */
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
-    }
-    
-    protected void onAfterRequest(RequestContext requestContext) {
-
-      super.onAfterRequest(requestContext);
-    }
-    
-   protected void processRequest(String pageName, javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws ServletException, IOException {
-      if( pageName.equals(IndexViewBean.PAGE_NAME)) {
-         clearResultModel();
-         clearQueryModel();
-      }
-      super.processRequest(pageName, request, response);
-   }
-    
+    }    
     
     protected void initializeRequestContext(RequestContext requestContext) {
 
         /* Always call super, unless specifically instructed not to. */
-        super.initializeRequestContext(requestContext);
-        
-        
+        super.initializeRequestContext(requestContext);     
 
         /* Create a ViewBeanManager with this context and the correct package. */
         ViewBeanManager vbm = new ViewBeanManager(requestContext, PACKAGE_NAME);
